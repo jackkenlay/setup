e35c581fe6a7b2b9479074ed6a5ca21920583986
Jack <jackkenlay@gmail.com>
2019-08-14 19:27:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+# Setup (on Linux)
+#
+# Get Curl    
+# sudo apt install curl -y  
 
+# cd;
+# mkdir -p setup
+# cd setup
+# curl -#L https://github.com/jackkenlay/setup/tarball/master | tar -xzv --strip-components 1 

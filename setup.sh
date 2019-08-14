fe16d6084469f9a3c21fcfecd9bd88ef97b864c7
Jack <jackkenlay@gmail.com>
2019-08-14 21:11:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,33 @@
 # Append to use this bashprofile from the normal one
 # If possible, install the other bits and pieces I use.
 
-# programs
-# chrome
-# node
-# npm
-# if mac (brew)
-# setup ColeMak keyboard
 
 echo "Starting Setup .sh"
-
 sudo apt update
-sudo apt install google-chrome-stable
+
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# node
+sudo apt install nodejs -y
+
+# npm
+sudo apt install npm -y
+
+# vscode
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update && sudo apt-get install ubuntu-make -y
+umake web visual-studio-code
+
+# git
+sudo apt install git -y
+
+# mysql
+# https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-18-04
+sudo apt install mysql-server -y
+sudo mysql_secure_installation
+
+
+# .gitconfig
+# setup ColeMak keyboard
